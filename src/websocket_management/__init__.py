d8771deed1f1759dc49e3dabf32dfc6bab7145b5
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-24 22:48:50 +0700

    Update __init__.py

--- src/websocket_management/__init__.py
+++ src/websocket_management/__init__.py
@@ -2,7 +2,7 @@ __all__ = [
     #"clean_up_closed_transactions", 
     #"count_and_delete_ohlc_rows",
     #"check_whether_db_reconciled_each_other",
-    #"check_whether_order_db_reconciled_each_other",
+    "check_whether_order_db_reconciled_each_other",
     "get_unrecorded_trade_and_order_id",
     #"reconciling_sub_account_and_db_open_orders"
     ]
