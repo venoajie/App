3ed2430e4f499b88209275984a33327ca8fed3e5
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-24 10:42:50 +0700

    Update transaction_log.py

--- src/transaction_management/deribit/transaction_log.py
+++ src/transaction_management/deribit/transaction_log.py
@@ -16,11 +16,13 @@ def telegram_bot_sendtext(bot_message, purpose: str = "general_error") -> None:
 
     return telegram_app.telegram_bot_sendtext(bot_message, purpose)
 
-async def saving_transaction_log (transaction_log_trading, 
-                                  archive_db_table,
-                                  transaction_log,
-                                  first_tick_fr_sqlite, 
-                                  ) -> None:
+async def saving_transaction_log (
+    transaction_log_trading, 
+    archive_db_table,
+    transaction_log,
+    first_tick_fr_sqlite, 
+    ) -> None:
+    
     """_summary_
 
     Args:
@@ -29,51 +31,52 @@ async def saving_transaction_log (transaction_log_trading,
     """
     
 
-    #log.debug (f"transaction_log {transaction_log}")
-    for transaction in transaction_log:
+    if transaction_log:
         
-        modified_dict= remove_dict_elements(transaction,"info")
-                    
-        timestamp_log= modified_dict ["timestamp"]
-        
-        type_log= modified_dict ["type"]
-                    
-        if timestamp_log > first_tick_fr_sqlite:
+        for transaction in transaction_log:
+            
+            modified_dict= remove_dict_elements(transaction,"info")
+                        
+            timestamp_log= modified_dict ["timestamp"]
+            
+            type_log= modified_dict ["type"]
+                        
+            if timestamp_log > first_tick_fr_sqlite:
 
-            custom_label= f"custom-{type_log.lower()}-{timestamp_log}"
-            
-            if "trade" in type_log or "delivery" in type_log:
-            
-                if "trade" in type_log:
+                custom_label= f"custom-{type_log.lower()}-{timestamp_log}"
                 
-                    tran_id_log= modified_dict ["trade_id"]
+                if "trade" in type_log or "delivery" in type_log:
+                
+                    if "trade" in type_log:
+                    
+                        tran_id_log= modified_dict ["trade_id"]
 
-                    instrument_name_log= modified_dict ["instrument_name"]
-                    
-                    column_list: str="label", "trade_id"
-                    
-                    from_sqlite_open= await get_query (archive_db_table, 
-                                                        instrument_name_log, 
-                                                        "all", 
-                                                        "all", 
-                                                        column_list)                                       
-                    
-                    label_log= [o["label"] for o in from_sqlite_open if tran_id_log in o["trade_id"]]
-                                                            
-                    if label_log !=[]:
-                        modified_dict.update({"label": label_log[0]})
-                    
-                    else:
+                        instrument_name_log= modified_dict ["instrument_name"]
+                        
+                        column_list: str="label", "trade_id"
+                        
+                        from_sqlite_open= await get_query (archive_db_table, 
+                                                            instrument_name_log, 
+                                                            "all", 
+                                                            "all", 
+                                                            column_list)                                       
+                        
+                        label_log= [o["label"] for o in from_sqlite_open if tran_id_log in o["trade_id"]]
+                                                                
+                        if label_log !=[]:
+                            modified_dict.update({"label": label_log[0]})
+                        
+                        else:
+                            modified_dict.update({"label": custom_label})
+                
+                    if "delivery" in type_log:
                         modified_dict.update({"label": custom_label})
-            
-                if "delivery" in type_log:
+                        
+                    #log.debug (f"transaction_log_json {modified_dict}")
+                    await insert_tables(transaction_log_trading, modified_dict)
+                
+                else:
                     modified_dict.update({"label": custom_label})
-                    
-                #log.debug (f"transaction_log_json {modified_dict}")
-                await insert_tables(transaction_log_trading, modified_dict)
-            
-            else:
-                modified_dict.update({"label": custom_label})
-                #log.debug (f"transaction_log_json {modified_dict}")
-                table= f"transaction_log_json"
-                await insert_tables(table, modified_dict)
+                    #log.debug (f"transaction_log_json {modified_dict}")
+                    table= f"transaction_log_json"
+                    await insert_tables(table, modified_dict)
