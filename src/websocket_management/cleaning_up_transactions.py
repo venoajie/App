a4f3792c9c5acc736e7cc4bc2da6b012854c9103
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-26 07:59:22 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -721,8 +721,7 @@ async def clean_up_closed_transactions(
     #prepare basic parameters for table query
 
     log.error(f" clean_up_closed_transactions {instrument_name} START")
-    log.error(f" transaction_all {transaction_all} ")
-    log.error(f" transaction_all { not transaction_all} ")
+    log.error(f" transaction_all is None {transaction_all is None} ")
     
     where_filter = f"trade_id"
 
@@ -742,6 +741,8 @@ async def clean_up_closed_transactions(
     else:      
         
         if transaction_all:
+            
+            log.error (f"AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
             
             transactions_all: list = [o for o in transaction_all \
             if instrument_name in o["instrument_name"]]
