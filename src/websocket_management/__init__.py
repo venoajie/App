756cfe9fc52340ae5948338ef4990023f7ab2798
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-28 07:56:57 +0700

    .

--- src/websocket_management/__init__.py
+++ src/websocket_management/__init__.py
@@ -3,6 +3,5 @@ from websocket_management.ws_management import (
     cancel_all,
     currency_inline_with_database_address,
     distribute_ticker_result_as_per_data_type,
-    get_futures_instruments,
     labelling_the_unlabelled_and_resend_it,
     )
