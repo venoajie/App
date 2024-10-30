bfa88bc541cbb6e943c838f96aba51b2e5bb9244
venoajie <59521823+venoajie@users.noreply.github.com>
2024-10-30 19:33:44 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -23,6 +23,167 @@ from datetime import datetime, timedelta
 import websockets
 
 
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# built ins
+from datetime import datetime, timedelta
+import asyncio
+import functools
+import os
+
+# installed
+from dataclassy import dataclass, fields
+from loguru import logger as log
+import json
+import orjson
+import tomli
+import websockets
+
+# user defined formula
+from configuration import id_numbering, config
+from configuration.label_numbering import get_now_unix_time
+from db_management.sqlite_management import (
+    querying_table,
+    executing_query_based_on_currency_or_instrument_and_strategy as get_query,
+    insert_tables,)
+from transaction_management.deribit.api_requests import (
+    ModifyOrderDb,)
+from transaction_management.deribit.telegram_bot import (
+    telegram_bot_sendtext,)
+from strategies.basic_strategy import(
+    is_label_and_side_consistent,)
+from strategies.hedging_spot import HedgingSpot  
+from transaction_management.deribit.api_requests import (
+    get_instruments,)
+from transaction_management.deribit.orders_management import (
+    saving_traded_orders,
+    saving_orders,)
+from utilities.number_modification import get_closest_value
+from utilities.pickling import (
+    replace_data,
+    read_data,)
+from utilities.system_tools import (
+    provide_path_for_file,
+    async_raise_error_message,
+    raise_error_message,
+    sleep_and_restart,)
+from utilities.string_modification import (
+    extract_currency_from_text,
+    remove_double_brackets_in_list,
+    remove_redundant_elements,)
+from websocket_management.ws_management import (
+    cancel_all,
+    currency_inline_with_database_address,
+    distribute_ticker_result_as_per_data_type,
+    get_futures_instruments,
+    labelling_the_unlabelled_and_resend_it,)
+from websocket_management.allocating_ohlc import (
+    ohlc_result_per_time_frame,
+    inserting_open_interest,)
+from websocket_management.cleaning_up_transactions import (
+    check_whether_order_db_reconciled_each_other,
+    check_whether_size_db_reconciled_each_other,
+    clean_up_closed_transactions,)
+
+@functools.lru_cache(maxsize=128)
+def memoization (sub_account) -> dict:
+    """_summary_
+    https://dbader.org/blog/python-memoization
+    https://rednafi.com/python/lru_cache_on_methods/
+    https://gist.github.com/Morreski/c1d08a3afa4040815eafd3891e16b945
+    https://www.tutorialspoint.com/clear-lru-cache-in-python
+
+    Args:
+        sub_account (_type_): _description_
+
+    Returns:
+        dict: _description_
+    """
+    return config.main_dotenv(sub_account)
+
+def parse_dotenv (sub_account) -> dict:
+    return config.main_dotenv(sub_account)
+
+def reading_from_pkl_data(
+    end_point, 
+    currency,
+    status: str = None
+    ) -> dict:
+    """ """
+
+    path: str = provide_path_for_file (end_point,
+                                      currency,
+                                      status)
+
+    data = read_data(path)
+
+    return data
+
+def get_config(file_name: str) -> list:
+    """ """
+    config_path = provide_path_for_file (file_name)
+    
+    try:
+        if os.path.exists(config_path):
+            with open(config_path, "rb") as handle:
+                read= tomli.load(handle)
+                return read
+    except:
+        return []
+
+
+def compute_notional_value(
+    index_price: float,
+    equity: float
+    ) -> float:
+    """ """
+    return index_price * equity
+
+
+async def update_db_pkl(
+    path, 
+    data_orders,
+    currency
+    ) -> None:
+
+    my_path_portfolio = provide_path_for_file (path,
+                                               currency)
+        
+    if currency_inline_with_database_address(
+        currency,
+        my_path_portfolio):
+        
+        replace_data (
+            my_path_portfolio, 
+            data_orders
+            )
+
+def get_settlement_period (strategy_attributes) -> list:
+    
+    return (remove_redundant_elements(
+        remove_double_brackets_in_list(
+            [o["settlement_period"]for o in strategy_attributes]))
+            )
+
+def get_index (
+    data_orders: dict, 
+    ticker: dict
+    ) -> float:
+
+    try:
+        index_price= data_orders["index_price"]
+        
+    except:
+        
+        index_price= ticker["index_price"]
+        
+        if index_price==[]:
+            index_price = ticker ["estimated_delivery_price"]
+        
+    return index_price
+
+
 class main:
     def __init__(
         self,
@@ -76,7 +237,7 @@ class main:
             while self.websocket_client.open:
                 message: bytes = await self.websocket_client.recv()
                 message: Dict = json.loads(message)
-                logging.info(message)
+                # logging.info(message)
 
                 if 'id' in list(message):
                     if message['id'] == 9929:
