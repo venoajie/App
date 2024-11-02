cbe97c97795dccb574777c6a6ba2f53c9f301161
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-02 08:12:37 +0700

    .

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -185,8 +185,12 @@ async def get_unrecorded_trade_from_transaction_log(
     my_trades_instrument_name_trade_id = [o["trade_id"] for o in my_trades_instrument_name]
     from_transaction_log_instrument_trade_id = [o["trade_id"] for o in from_transaction_log_instrument]  
                        
-    unrecorded_trade_id = get_unique_elements(from_transaction_log_instrument_trade_id, 
+    if my_trades_instrument_name_trade_id:
+        unrecorded_trade_id = get_unique_elements(from_transaction_log_instrument_trade_id, 
                                               my_trades_instrument_name_trade_id)
+                   
+    else:
+        unrecorded_trade_id = from_transaction_log_instrument_trade_id
     
     log.debug(f"unrecorded_trade_from_transaction_log {unrecorded_trade_id}")
 
