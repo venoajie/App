fe2c0538a5b821640108ddac9284b7ebc035557d
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-06 06:26:44 +0700

    Update cleaning_up_transactions.py

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -249,7 +249,7 @@ def check_whether_size_db_reconciled_each_other(
 
         from_transaction_log_instrument =([o for o in from_transaction_log \
             if o["instrument_name"] == instrument_name])
-        log.info (f"from_transaction_log_instrument {from_transaction_log_instrument}")
+        
         
         #timestamp could be double-> come from combo transaction. hence, trade_id is used to distinguish
         # other possibilities (instrument name beyond those in config):
@@ -302,6 +302,7 @@ def check_whether_size_db_reconciled_each_other(
         different_from_all_db_sources = current_position_log == sub_account_size_instrument  == sum_my_trades_instrument 
         
         if not different_from_all_db_sources:
+            log.info (f"from_transaction_log_instrument {from_transaction_log_instrument}")
             log.critical(f"{instrument_name} different_from_all_db_sources {different_from_all_db_sources} different_from_sub_accont_and_trans_log {different_from_sub_accont_and_trans_log} sum_my_trades_currency {sum_my_trades_instrument}  sub_account_size_instrument {sub_account_size_instrument} current_position_log {current_position_log}")
 
         # combining result
