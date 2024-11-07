4e9d28664e75832b9389cfff72fa47f34968f10e
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-07 08:44:20 +0700

    Update combo_auto.py

--- src/strategies/combo_auto.py
+++ src/strategies/combo_auto.py
@@ -334,9 +334,9 @@ class ComboAuto (BasicStrategy):
                             cancel_id=None if not cancel_allowed else cancel_id
         )
                         
-                        will_be_closed.append(result.copy())
-                        log.warning (f"result {result}")
-                        log.debug (f"will_be_closed {will_be_closed}")
+                will_be_closed.append(result.copy())
+                log.warning (f"result {result}")
+                log.debug (f"will_be_closed {will_be_closed}")
 
             log.critical (f"exit_params {will_be_closed}")
         
