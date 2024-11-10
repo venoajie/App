fa6bd58d7f82c42ac50093502ecd019b0806b3db
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-10 15:47:42 +0700

    .

---
+++ src/app_st.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# built insfrom datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pandas_datareader as pdr
+import streamlit as st
+
+
+def main():
+    st.title("app_st")
+
+
+if __name__ == main():
+    main()
