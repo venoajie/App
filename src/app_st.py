291041b9de5eb0f1dda60d668bd353fdb1a9efba
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-10 16:55:42 +0700

    Update app_st.py

--- src/app_st.py
+++ src/app_st.py
@@ -41,7 +41,7 @@ async def fetch_data(url):
 
 async def main():
     data = await fetch_data('https://www.deribit.com/api/v2/public/ticker?instrument_name=BTC-PERPETUAL')
-    st.write(data)
+    st.dataframe(data)
 
 if __name__ == '__main__':
     asyncio.run(main())
