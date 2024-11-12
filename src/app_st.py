fd4741d9fc544912d593c106845c09231da2038d
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-12 11:37:25 +0700

    Update app_st.py

--- src/app_st.py
+++ src/app_st.py
@@ -5,17 +5,178 @@
 import asyncio
 import numpy as np
 import pandas as pd
-
+import os
+import tomli
+import datetime
+from loguru import logger as log
 import streamlit as st
 import httpx
 import asyncio
+from transaction_management.deribit.api_requests import get_tickers, get_currencies,get_instruments
+
+from utilities.string_modification import (
+    remove_dict_elements,
+    remove_double_brackets_in_list,
+    remove_redundant_elements,)
 
 
+from utilities.pickling import (
+    read_data,
+    replace_data,)
+from utilities.system_tools import (
+    provide_path_for_file,
+    async_raise_error_message,
+    raise_error_message,)
+
+from websocket_management.ws_management import (
+    get_futures_instruments,)
 from db_management.sqlite_management import (
     executing_query_based_on_currency_or_instrument_and_strategy as get_query,)
 
+def get_config(file_name: str) -> list:
+    """ """
+    config_path = provide_path_for_file (file_name)
+    
+    try:
+        if os.path.exists(config_path):
+            with open(config_path, "rb") as handle:
+                read= tomli.load(handle)
+                return read
+    except:
+        return []
+
+
+def reading_from_pkl_data(
+    end_point, 
+    currency,
+    status: str = None
+    ) -> dict:
+    """ """
+
+    path: str = provide_path_for_file (end_point,
+                                      currency,
+                                      status)
+
+    return read_data(path)
+
+
+
+
+async def get_currencies_from_deribit() -> float:
+    """ """
+
+    return await get_currencies()
+
+
 trade_db_table= "my_trades_all_json"
 
+
+async def get_instruments_from_deribit(currency) -> float:
+    """ """
+
+    result = await get_instruments(currency)
+
+    return result
+
+def get_settlement_period (strategy_attributes) -> list:
+    
+    return (remove_redundant_elements(
+        remove_double_brackets_in_list(
+            [o["settlement_period"]for o in strategy_attributes]))
+            )
+# registering strategy config file    
+file_toml = "config_strategies.toml"
+
+# parsing config file
+config_app = get_config(file_toml)
+
+# get tradable strategies
+tradable_config_app = config_app["tradable"]
+
+# get tradable currencies
+currencies= [o["spot"] for o in tradable_config_app] [0]
+
+strategy_attributes = config_app["strategies"]
+                        
+active_strategies =   [o["strategy_label"] for o in strategy_attributes \
+    if o["is_active"]==True]
+
+# get strategies that have not short/long attributes in the label 
+non_checked_strategies =   [o["strategy_label"] for o in strategy_attributes \
+    if o["non_checked_for_size_label_consistency"]==True]
+
+cancellable_strategies =   [o["strategy_label"] for o in strategy_attributes \
+    if o["cancellable"]==True]
+
+trade_db_table= "my_trades_all_json"
+
+order_db_table= "orders_all_json"         
+                
+settlement_periods= get_settlement_period (strategy_attributes)
+                
+futures_instruments= get_futures_instruments (currencies,
+                                                    settlement_periods)  
+
+active_futures = futures_instruments["active_futures"]   
+
+active_combo_perp = futures_instruments["active_combo_perp"]  
+
+
+async def get_ticker():
+    
+    log.critical("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+                
+    
+    ticker = []
+    for combo in active_combo_perp:
+        
+        instrument_name = combo["instrument_name"]
+            
+        instrument_ticker: list = await get_tickers(
+        instrument_name
+        )
+        
+        elements_to_be_removed=["stats", "combo_state", "state","timestamp"]
+        
+        modified_dict = remove_dict_elements(
+                instrument_ticker,
+                "stats"
+                )
+        modified_dict = remove_dict_elements(
+                modified_dict,
+                "combo_state"
+                )
+        modified_dict = remove_dict_elements(
+                modified_dict,
+                "state"
+                )
+        modified_dict = remove_dict_elements(
+                modified_dict,
+                "timestamp"
+                )
+        log.error (modified_dict)
+        
+        
+        ticker.append (modified_dict)
+
+    return (ticker)
+
+@st.experimental_fragment(run_every=2)
+async def rerun_ticker():
+    
+    """
+    https://st-experimental-fragment.streamlit.app/Job_status
+    """
+
+    data_ticker = await (get_ticker())
+    df = pd.DataFrame (data_ticker)
+    edited_df = st.data_editor(df)
+    st.subheader("Ticker")
+    st.dataframe(data_ticker)
+    st.markdown("##")
+
+    st.caption(f"Last updated {datetime.datetime.now()}")        
+    st.rerun()
 
 async def get_db_table():
                 
@@ -44,30 +205,40 @@ async def main():
     #data = await fetch_data('https://www.deribit.com/api/v2/public/ticker?instrument_name=BTC-PERPETUAL')
     #st.dataframe(data["result"])
 
-    data = await get_db_table()
-    st.title("Current ")
-
-    data_order = await get_open_orders()
-    st.title("Current ")
-    
-    st.header("Current Positions")
-    st.table()
-    
-    st.markdown("""---""")
-
-    st.subheader("Positions")
-    st.dataframe(data)
-    st.markdown("##")
-    
-    left_column, right_column = st.columns(2)
+    try:
+        st.title("Current ")
         
-    with left_column:
-        st.subheader("Trades")
-        st.dataframe (data)
-    with right_column:
-        st.subheader("Open orders")
-        st.dataframe (data_order)
+        data = await get_db_table()
+
+        data_order = await get_open_orders()
+        st.title("Current ")
+        
+        st.header("Current Positions")
+        st.table()
+        
+        st.markdown("""---""")
+
+        st.subheader("Positions")
+        st.dataframe(data)
+        st.markdown("##")
+        
+        left_column, right_column = st.columns(2)
+            
+        with left_column:
+            st.subheader("Trades")
+            st.dataframe (data)
+        with right_column:
+            st.subheader("Open orders")
+            st.dataframe (data_order)
     
+        await  rerun_ticker ()
+        #st.rerun()
+
+        
+        
+    except Exception as error:
+        await async_raise_error_message(error)
+        
 if __name__ == '__main__':
     
     st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
@@ -81,5 +252,5 @@ if __name__ == '__main__':
                 </style>
                 """
     st.markdown(hide_st_style, unsafe_allow_html=True)
-    
+
     asyncio.run(main())
