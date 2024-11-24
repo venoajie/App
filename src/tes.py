c38c6e3da3f54dc944954f595f770389841afca7
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-24 15:56:53 +0700

    Update tes.py

--- src/tes.py
+++ src/tes.py
@@ -17,14 +17,15 @@ users_data = [
 dtype = [("user_id", "i4"), ("plan_type", "U10"), ("data_usage", "i4")]
 np_users_data = np.array([tuple(user.values()) for user in users_data], dtype=dtype)
 print (np_users_data)
-nump = [{"liquidity": "M", "order_type": "limit", "risk_reducing": False}, 
-        {"liquidity": "M", "order_type": "limit", "risk_reducing": False}
+nump = [{"liquidity": "M", "order_type": "limit", "tick_direction": 3,"risk_reducing": False}, 
+        {"liquidity": "M", "order_type": "limit", "tick_direction": 3,"risk_reducing": False}
        ]
 vtr1 = np.array(nump)   
 
 dtype = [
     ("liquidity","U1"),
     ("risk_reducing", "bool"),
+    ("tick_direction", "14"), 
     ("order_type", "U5")
     ]
 
