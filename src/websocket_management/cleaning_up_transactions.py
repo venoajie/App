140448568751184e7c048e18d95df7d897c4a1ee
venoajie <59521823+venoajie@users.noreply.github.com>
2024-11-27 05:23:21 +0700

    .

--- src/websocket_management/cleaning_up_transactions.py
+++ src/websocket_management/cleaning_up_transactions.py
@@ -374,30 +374,16 @@ def get_unrecorded_trade_transactions(
     """
      
     from_transaction_log_instrument_trade_id = sorted([o["trade_id"] for o in from_transaction_log_instrument] )
-    from_transaction_log_instrument_amount = ([o["amount"] for o in from_transaction_log_instrument] )
     
     #log.error (f"my_trades_instrument_name {my_trades_instrument_name}")
-    log.warning (f"from_transaction_log_instrument_amount {from_transaction_log_instrument_amount}")
-    log.debug (f"from_transaction_log_instrument_trade_id {from_transaction_log_instrument_trade_id}")
-    
     if direction == "from_trans_log_to_my_trade":
 
         if my_trades_instrument_name:
             my_trades_instrument_name_trade_id = sorted([o["trade_id"] for o in my_trades_instrument_name])
-            my_trades_instrument_name_amount= ([o["amount"] for o in my_trades_instrument_name])
-            
-            log.warning (f"my_trades_instrument_name_amount {my_trades_instrument_name_amount}")
-            log.debug (f"my_trades_instrument_name_trade_id {my_trades_instrument_name_trade_id}")
             
             if my_trades_instrument_name_trade_id:
                 unrecorded_trade_id = get_unique_elements(from_transaction_log_instrument_trade_id, 
                                                     my_trades_instrument_name_trade_id)
-                log.debug(f"unrecorded_trade_from_transaction_log {unrecorded_trade_id}")
-                        
-                unrecorded_trade_id = get_unique_elements(my_trades_instrument_name_trade_id, 
-                                                    from_transaction_log_instrument_trade_id)
-                log.debug(f"unrecorded_trade_from_transaction_log {unrecorded_trade_id}")
-                        
         else:
             unrecorded_trade_id = from_transaction_log_instrument_trade_id
         
