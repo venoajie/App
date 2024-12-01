96c79423662cad299d235ac6b0e4f4143d0ab203
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-01 10:02:10 +0700

    .

--- src/data_cleaning/managing_closed_transactions.py
+++ src/data_cleaning/managing_closed_transactions.py
@@ -217,7 +217,7 @@ def get_unrecorded_trade_transactions(
     Returns:
         _type_: _description_
     """
-     
+    log.info (f"from_transaction_log_instrument {from_transaction_log_instrument}") 
     from_transaction_log_instrument_trade_id = sorted([o["trade_id"] for o in from_transaction_log_instrument] )
     
     #log.error (f"my_trades_instrument_name {my_trades_instrument_name}")
