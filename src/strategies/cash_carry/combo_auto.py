9c6587cce93c22c68a5052bb728697db4e1b7078
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-03 05:48:39 +0700

    Update combo_auto.py

--- src/strategies/cash_carry/combo_auto.py
+++ src/strategies/cash_carry/combo_auto.py
@@ -371,6 +371,7 @@ class ComboAuto (BasicStrategy):
             
     def __post_init__(self):
         log.error ( (-820 if "BTC" in self.ticker_perpetual["instrument_name"] else 0) )
+        log.critical (get_delta (self.my_trades_currency_strategy) )
         self.delta: float = get_delta (self.my_trades_currency_strategy) (-820 if "BTC" in self.ticker_perpetual["instrument_name"] else 0) 
         self.basic_params: str = BasicStrategy (
             self.strategy_label,
