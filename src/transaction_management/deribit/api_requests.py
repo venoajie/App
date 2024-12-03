503e24a0baa14808b988a429dcf4ae3c2a0e552e
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-03 13:11:09 +0700

    Update api_requests.py

--- src/transaction_management/deribit/api_requests.py
+++ src/transaction_management/deribit/api_requests.py
@@ -181,6 +181,7 @@ class SendApiRequest:
         log.debug (f"otoco_config {otoco_config}")
         if otoco_config:
             params.update({"otoco_config": otoco_config})
+        log.debug (f"params {params}")
         
         result = None
         
