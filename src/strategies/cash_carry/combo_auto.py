45b527dec8f8af1509e0eea536fb9b1e53d77347
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-10 17:49:38 +0700

    .

--- src/strategies/cash_carry/combo_auto.py
+++ src/strategies/cash_carry/combo_auto.py
@@ -672,8 +672,12 @@ class ComboAuto (BasicStrategy):
                                    
                         #log.debug  (f" {premium_pct > tp_threshold } basic_ordering_is_ok {basic_ordering_is_ok } current_premium > 0 {current_premium} {current_premium > 0} current_premium < transactions_premium {current_premium < transactions_premium}")
                                         
+                        log.warning (f"transactions {transactions}")
+                        log.warning (f"orders_currency {orders_currency}")
                         len_label = len([o["label"] for o in orders_currency\
                             if label_integer in o["label"]])
+                        
+                        log.debug (f"len_label {len_label}")
                         
                         if len_label == 0\
                             and premium_pct > tp_threshold \
