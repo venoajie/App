e6930ed1f30a6b031203b928af1f484b73215180
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-10 11:50:37 +0700

    Update combo_auto.py

--- src/strategies/cash_carry/combo_auto.py
+++ src/strategies/cash_carry/combo_auto.py
@@ -706,8 +706,6 @@ class ComboAuto (BasicStrategy):
                                                     
                                 order_allowed = True
          
-        log.debug  (f"order_allowed {order_allowed}")
-        log.debug  (f"order_parameters {exit_params}")
         return dict(
             order_allowed= order_allowed,
             order_parameters=(
