bbb7130fd35d0df9ce02b7fa865aa6e95fccc3aa
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-12 18:01:02 +0700

    .

--- src/data_cleaning/managing_closed_transactions.py
+++ src/data_cleaning/managing_closed_transactions.py
@@ -437,119 +437,124 @@ async def clean_up_closed_transactions(
     #log.critical(f" {"clean_up_closed_transactions".upper()} {instrument_name} START")
     
     where_filter = f"trade_id"
+    
+    try:
 
-    if transaction_all is None:
-        column_list: str= "instrument_name","label", "amount", where_filter, "is_open"
-        
-        #querying tables
-        transaction_all: list = await get_query(
-            trade_table,
-            currency, 
-            "all",
-            "all",
-            column_list,
-            )
-           
-        transaction_all = [o for o in transaction_all\
-            if o["label"] is not None]        
+        if transaction_all is None:
+            column_list: str= "instrument_name","label", "amount", where_filter, "is_open"
+            
+            #querying tables
+            transaction_all: list = await get_query(
+                trade_table,
+                currency, 
+                "all",
+                "all",
+                column_list,
+                )
+            
+            transaction_all = [o for o in transaction_all\
+                if o["label"] is not None]        
 
-    else:      
-        
+        else:      
+            
+            if transaction_all:
+                            
+                transaction_all: list = [o for o in transaction_all \
+                    if currency in o["instrument_name"]]
+
+        # filtered transactions with closing labels
         if transaction_all:
-                        
-            transaction_all: list = [o for o in transaction_all \
-                if currency in o["instrument_name"]]
-
-    # filtered transactions with closing labels
-    if transaction_all:
-        
-        transaction_with_closed_labels = get_transactions_with_closed_label(transaction_all)
-        
-        #log.debug(f"transaction_with_closed_labels {transaction_with_closed_labels}")
-                               
-        labels_only = remove_redundant_elements([o["label"] for o in transaction_with_closed_labels])
-
-        if transaction_with_closed_labels:
-
-            for label in labels_only:
-                
-                transactions_under_label_main = get_label_main(
-                    transaction_all,
-                    label
-                    )
-                
-                label_integer = get_label_integer(label)
-                
-                closed_transactions_all= transactions_under_label_int(
-                    label_integer,
-                    transactions_under_label_main
-                    )
-
-                size_to_close = closed_transactions_all["summing_closed_transaction"]
-                
-                transaction_closed_under_the_same_label_int = closed_transactions_all["closed_transactions"]
-                
-                #log.error(f"closed_transactions_all {closed_transactions_all}")
-
-                if size_to_close == 0:
-                    
-                    instrument_name_under_the_same_label_int = [o["instrument_name"] for o in transaction_closed_under_the_same_label_int]
-                    
-                    for instrument_name in instrument_name_under_the_same_label_int:
-                        
-                        instrument_transactions = ([ o for  o in transaction_closed_under_the_same_label_int\
-                            if instrument_name in o["instrument_name"]])
-                        
-                        sum_instrument_transactions = sum([o["amount"] for o in instrument_transactions])
-                        
-                        if sum_instrument_transactions == 0:
-                            #log.info(F" instrument_transactions {instrument_transactions}")
-                            
-                            await closing_one_to_one(
-                                instrument_transactions,
-                                where_filter,
-                                trade_table,
-                                )
-
-                if size_to_close != 0:
-                    
-                    open_label =  ([o for o in transaction_closed_under_the_same_label_int\
-                        if "open" in o["label"]])
-                    
-                    transactions = []
-                    for open_transaction in open_label:
-                        
-                        open_transaction_size = open_transaction["amount"]
-                        open_transaction_instrument_name = open_transaction["instrument_name"]
-                        
-                        closed_transaction_with_same_size_as_open_label = [
-                        o for o in transaction_closed_under_the_same_label_int\
-                            if "closed" in o["label"] \
-                                and abs(o["amount"]) == abs(open_transaction_size)]
-                        
-                        if closed_transaction_with_same_size_as_open_label:
-                            
-                            for closed_transaction in closed_transaction_with_same_size_as_open_label:
-                                                            
-                                closed_transaction_size = closed_transaction["amount"]
-                                closed_transaction_instrument_name = closed_transaction["instrument_name"]
+            
+            transaction_with_closed_labels = get_transactions_with_closed_label(transaction_all)
+            
+            #log.debug(f"transaction_with_closed_labels {transaction_with_closed_labels}")
                                 
-                                if closed_transaction_instrument_name == open_transaction_instrument_name\
-                                    and closed_transaction_size + open_transaction_size == 0:
-                                    
-                                    transactions.append (open_transaction)
-                                    transactions.append (closed_transaction)
-                                    
-                                    #log.critical (f"transactions {transactions}")
+            labels_only = remove_redundant_elements([o["label"] for o in transaction_with_closed_labels])
+
+            if transaction_with_closed_labels:
+
+                for label in labels_only:
+                    
+                    transactions_under_label_main = get_label_main(
+                        transaction_all,
+                        label
+                        )
+                    
+                    label_integer = get_label_integer(label)
+                    
+                    closed_transactions_all= transactions_under_label_int(
+                        label_integer,
+                        transactions_under_label_main
+                        )
+
+                    size_to_close = closed_transactions_all["summing_closed_transaction"]
+                    
+                    transaction_closed_under_the_same_label_int = closed_transactions_all["closed_transactions"]
+                    
+                    #log.error(f"closed_transactions_all {closed_transactions_all}")
+
+                    if size_to_close == 0:
+                        
+                        instrument_name_under_the_same_label_int = [o["instrument_name"] for o in transaction_closed_under_the_same_label_int]
+                        
+                        for instrument_name in instrument_name_under_the_same_label_int:
+                            
+                            instrument_transactions = ([ o for  o in transaction_closed_under_the_same_label_int\
+                                if instrument_name in o["instrument_name"]])
+                            
+                            sum_instrument_transactions = sum([o["amount"] for o in instrument_transactions])
+                            
+                            if sum_instrument_transactions == 0:
+                                #log.info(F" instrument_transactions {instrument_transactions}")
                                 
-                                    await closing_one_to_one(
-                                        transactions,
-                                        where_filter,
-                                        trade_table,
-                                        )
+                                await closing_one_to_one(
+                                    instrument_transactions,
+                                    where_filter,
+                                    trade_table,
+                                    )
+
+                    if size_to_close != 0:
+                        
+                        open_label =  ([o for o in transaction_closed_under_the_same_label_int\
+                            if "open" in o["label"]])
+                        
+                        transactions = []
+                        for open_transaction in open_label:
+                            
+                            open_transaction_size = open_transaction["amount"]
+                            open_transaction_instrument_name = open_transaction["instrument_name"]
+                            
+                            closed_transaction_with_same_size_as_open_label = [
+                            o for o in transaction_closed_under_the_same_label_int\
+                                if "closed" in o["label"] \
+                                    and abs(o["amount"]) == abs(open_transaction_size)]
+                            
+                            if closed_transaction_with_same_size_as_open_label:
+                                
+                                for closed_transaction in closed_transaction_with_same_size_as_open_label:
+                                                                
+                                    closed_transaction_size = closed_transaction["amount"]
+                                    closed_transaction_instrument_name = closed_transaction["instrument_name"]
                                     
-                                    if closed_transaction_instrument_name == open_transaction_instrument_name:
+                                    if closed_transaction_instrument_name == open_transaction_instrument_name\
+                                        and closed_transaction_size + open_transaction_size == 0:
                                         
-                                        break
-                        
+                                        transactions.append (open_transaction)
+                                        transactions.append (closed_transaction)
+                                        
+                                        #log.critical (f"transactions {transactions}")
+                                    
+                                        await closing_one_to_one(
+                                            transactions,
+                                            where_filter,
+                                            trade_table,
+                                            )
+                                        
+                                        if closed_transaction_instrument_name == open_transaction_instrument_name:
                                             
+                                            break
+                            
+                                                
+    except Exception as error:
+        log.critical (f"error {error}")
+
