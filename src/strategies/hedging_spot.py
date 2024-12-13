93e27be39f6565933fdc04e2e5f7a56f70a8e3fa
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-13 08:27:23 +0700

    Update hedging_spot.py

--- src/strategies/hedging_spot.py
+++ src/strategies/hedging_spot.py
@@ -184,8 +184,8 @@ def check_if_minimum_waiting_time_has_passed(
         threshold
     )
     
-    #log.warning (f"minimum_waiting_time_has_passed {minimum_waiting_time_has_passed} time_interval {time_interval}")
-    #log.warning (f"server_time {server_time} max_tstamp_orders {timestamp}")
+    log.warning (f"minimum_waiting_time_has_passed {minimum_waiting_time_has_passed} time_interval {time_interval}")
+    log.warning (f"server_time {server_time} max_tstamp_orders {timestamp} threshold {threshold}")
     
     if minimum_waiting_time_has_passed:
         cancel_allowed: bool = True
