0fab1896a45524edf7cf28b6848e33f4e12570a2
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-15 08:53:26 +0700

    Update uji_hapus.py

--- src/uji_hapus.py
+++ src/uji_hapus.py
@@ -103,7 +103,7 @@ def ema_list(x, ratio):
 table_1 = f"ohlc1_btc_perp_json" 
 ohlc_1_high_9 = asyncio. run(cleaned_up_ohlc("high", table_1, 10))
 print (ohlc_1_high_9)
-ohlc_1_high_9 = [o["ohlc"] for o in ohlc_1_high_9]
+ohlc_1_high_9 = ohlc_1_high_9["ohlc"] 
 print (ohlc_1_high_9)
 ema= ema_list(ohlc_1_high_9,ratio)
 
