efa8c4bfb3df61ead739c3e835c190ed1c510ca6
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-22 04:40:08 +0700

    Update candles_analysis.py

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -23,7 +23,7 @@ def analysis_based_on_length(
         print(candles_summary)
         log.error (f"candles_summary.dtype {candles_summary.dtype} candles_summary.shape {candles_summary.shape} candles_summary.ndim {candles_summary.ndim} candles_summary.size {candles_summary.size}")
         
-        body_length =candles_summary[0:2, 2]
+        body_length =candles_summary[-1, :]
         
         log.warning (body_length)
     
