efe9492f43c945b6f56094e36fdc2b69f09cc652
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-21 18:31:41 +0700

    .

---
+++ src/market_understanding/price_action/candles_analysis.py
@@ -0,0 +1,30 @@
+from db_management import sql_executing_queries
+import asyncio
+from loguru import logger as log
+
+async def get_dataframe_from_ohlc_tables(tables: str = "ohlc60_eth_perp_json"):
+    """_summary_
+    https://www.tradingview.com/script/uuinZwsR-Big-Bar-Strategy/
+        Args:
+            tables (str, optional): _description_. Defaults to 'ohlc60_eth_perp_json'.
+
+        Returns:
+            _type_: _description_
+    """
+    import pandas as pd
+
+    barsizeThreshold = 0.5
+    period = 10
+    mult = 2
+    pd.set_option("display.max_rows", None)
+    res = await sql_executing_queries.querying_tables_item_data(tables)
+    df = pd.DataFrame(res)
+    df["candle_size"] = df["high"] - df["low"]
+    df["body_size"] = abs(df["open"] - df["close"])
+    df["candle_size_avg"] = df["candle_size"].rolling(period).mean()
+    df["bigbar"] = (df["candle_size"] >= df["candle_size_avg"] * mult) & (
+        df["body_size"] > df["candle_size"] * barsizeThreshold
+    )
+    print(df)
+
+    return df
