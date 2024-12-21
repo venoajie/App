f84f5318978528b9f4e2e3305ca4ba21e05eca3f
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-21 12:01:26 +0700

    Update tes.py

--- src/tes.py
+++ src/tes.py
@@ -138,9 +138,11 @@ for currency in currencies:
             
         log.warning (ohlc)
         np_users_data = np.array(ohlc)
+        log.warning (np_users_data)
 
         np_data = np.array([tuple(user.values()) for user in np_users_data], dtype=dtype)
+        log.warning (np_data)
 
-        three_dim_sequence = (get_candles_size.my_generator_candle(np,np_users_data[1:],3))
+        three_dim_sequence = (get_candles_size.my_generator_candle(np,np_data[1:],3))
         
         log.error (f"three_dim_sequence {three_dim_sequence}")
