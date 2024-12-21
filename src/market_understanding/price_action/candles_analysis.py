fe6cecb1eece6d1cf2cddcb85a5a0568e64385c7
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-22 04:53:23 +0700

    .

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -3,6 +3,7 @@ import asyncio
 from loguru import logger as log
 
 def analysis_based_on_length(
+    np: object,
     candles_data_instrument: list,
     resolutions: list):
     """_summary_
@@ -19,12 +20,14 @@ def analysis_based_on_length(
         
         data_per_resolution = [o for o in candles_data_instrument\
             if resolution == o["resolution"]]
-        candles_summary = [o["candles_summary"] for o in data_per_resolution][0]
-        print(candles_summary)
-        log.error (f"candles_summary.dtype {candles_summary.dtype} candles_summary.shape {candles_summary.shape} candles_summary.ndim {candles_summary.ndim} candles_summary.size {candles_summary.size}")
+
+        candles_arrays = [o["candles_summary"] for o in data_per_resolution][0]
+
+        body_length = candles_arrays [-1, :, 3] #(last_column_third_row)
+        mean_body_length = np.average(body_length)
         
-        body_length =candles_summary[-1, :,2]
-        
+        print(candles_arrays)
         log.warning (body_length)
+        log.warning (mean_body_length)
     
     return 
