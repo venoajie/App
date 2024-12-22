24a500cc6f1807d3d2ba1a0b10fd88528bcc12c9
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-22 18:01:42 +0700

    .

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -41,3 +41,59 @@ def analysis_based_on_length(
             )
 )
             
+
+
+def get_market_condition(
+    candles_data,
+    currency_upper
+    ):
+    """
+    """
+    candles_data_instrument = [o for o in candles_data \
+                                                if currency_upper in o["instrument_name"]]
+    
+    candle_60 = [o["candles_analysis"] for o in candles_data_instrument if o["resolution"] == 60]
+    candle_60_type = np.sum([o["candle_type"] for o in candle_60])
+    candle_60_is_long = np.sum([o["is_long_body"] for o in candle_60])
+    
+    candle_5 = [o["candles_analysis"] for o in candles_data_instrument if o["resolution"] == 5]
+    candle_5_type = np.sum([o["candle_type"] for o in candle_5])
+    candle_5_is_long = np.sum([o["is_long_body"] for o in candle_5])
+    
+    candle_15 = [o["candles_analysis"] for o in candles_data_instrument if o["resolution"] == 15]
+    candle_15_type = np.sum([o["candle_type"] for o in candle_15])
+    candle_15_is_long = np.sum([o["is_long_body"] for o in candle_15])
+    
+    log.warning (candle_60)
+    log.debug (candle_5)
+    log.debug (candle_60_type)
+    log.warning (candle_15)
+    
+    candle_60_long_body_more_than_2 = candle_60_is_long >= 2
+    
+    candle_60_no_long = candle_60_is_long == 0
+    
+    strong_bullish = candle_60_type >= 2 and candle_60_long_body_more_than_2
+    
+    bullish = candle_15_type > 2 and candle_60_is_long >= 2
+    
+    weak_bullish = candle_5_type > 2
+    
+    neutral = candle_60_no_long and candle_15_is_long == 0 and candle_5_is_long == 0
+    
+    weak_bearish = True
+    
+    bearish = candle_15_type <= -2 and candle_60_is_long >= 2
+    
+    strong_bearish = candle_60_type <= -2 and candle_60_long_body_more_than_2
+    
+    return dict(
+                strong_bullish = strong_bullish,
+                bullish = bullish,
+                weak_bullish = weak_bullish,
+                neutral = neutral,
+                weak_bearish = weak_bearish,
+                bearish = bearish,
+                strong_bearish = strong_bearish,
+                )
+
