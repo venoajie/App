d87fc3f58494c1582a2343c76ef66ae73f3b3eda
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-22 12:25:11 +0700

    .

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -4,8 +4,7 @@ from loguru import logger as log
 
 def analysis_based_on_length(
     np: object,
-    candles_data_instrument: list,
-    resolutions: list):
+    data_per_resolution: int):
     """_summary_
     https://www.tradingview.com/script/uuinZwsR-Big-Bar-Strategy/
         Args:
@@ -15,32 +14,24 @@ def analysis_based_on_length(
             _type_: _description_
     """
     
-    
-    for resolution in resolutions:
-        
-        log.critical (resolution)
-        
-        data_per_resolution = [o for o in candles_data_instrument\
-            if resolution == o["resolution"]]
+    candles_arrays = [o["candles_summary"] for o in data_per_resolution][0]
 
-        candles_arrays = [o["candles_summary"] for o in data_per_resolution][0]
-
-        candle_type = candles_arrays [-1, :, 0] #(last_column_third_row)
-        wicks_up = candles_arrays [-1, :, 1] #(last_column_third_row)
-        wicks_down = candles_arrays [-1, :, 2] #(last_column_third_row)
-        body_size = candles_arrays [-1, :, 3] #(last_column_third_row)
-        body_length = candles_arrays [-1, :, 4] #(last_column_third_row)
-        is_long_body = candles_arrays [-1, :, 5] #(last_column_third_row)
-        avg_body_length = np.average(body_length)
-        body_length_exceed_average = body_length > avg_body_length
-        #print(candles_arrays)
-        log.warning (f"candle_type {candle_type}")
-        log.warning (f"wicks_up {wicks_up}")
-        log.warning (f"wicks_down {wicks_down}")
-        log.warning (f"body_size {body_size}")
-        log.warning (f"body_length {body_length}")
-        log.warning (f"is_long_body {is_long_body}")
-        log.warning (f" avg_body_length {avg_body_length}")
-        log.warning (f" body_length_exceed_average {body_length_exceed_average}")
+    candle_type = candles_arrays [-1, :, 0] #(last_column_third_row)
+    wicks_up = candles_arrays [-1, :, 1] #(last_column_third_row)
+    wicks_down = candles_arrays [-1, :, 2] #(last_column_third_row)
+    body_size = candles_arrays [-1, :, 3] #(last_column_third_row)
+    body_length = candles_arrays [-1, :, 4] #(last_column_third_row)
+    is_long_body = candles_arrays [-1, :, 5] #(last_column_third_row)
+    avg_body_length = np.average(body_length)
+    body_length_exceed_average = body_length > avg_body_length
+    #print(candles_arrays)
+    log.warning (f"candle_type {candle_type}")
+    log.warning (f"wicks_up {wicks_up}")
+    log.warning (f"wicks_down {wicks_down}")
+    log.warning (f"body_size {body_size}")
+    log.warning (f"body_length {body_length}")
+    log.warning (f"is_long_body {is_long_body}")
+    log.warning (f" avg_body_length {avg_body_length}")
+    log.warning (f" body_length_exceed_average {body_length_exceed_average}")
     
     return 
