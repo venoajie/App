f3899f20a20cec60c0a79da33645e3ec71d81112
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-23 13:24:10 +0700

    Update reconciling_db.py

--- src/data_cleaning/reconciling_db.py
+++ src/data_cleaning/reconciling_db.py
@@ -385,38 +385,40 @@ async def reconciling_orders(
 
             orders_instrument_name = remove_redundant_elements([o["instrument_name"] for o in sub_account_orders  ])
                     
-        for instrument_name in orders_instrument_name:
+        if orders_instrument_name:
             
-            currency = extract_currency_from_text (instrument_name)
+            for instrument_name in orders_instrument_name:
+                
+                currency = extract_currency_from_text (instrument_name)
 
-            len_order_is_reconciled_each_other =  check_whether_order_db_reconciled_each_other (
-                sub_account,
-                instrument_name,
-                orders_currency)
-            
-            if not len_order_is_reconciled_each_other:
-                               
-                sub_account_instrument_name = [o for o in sub_account_orders \
-                    if instrument_name in o["instrument_name"]]
-                        
-                where_filter = f"instrument_name"
-                
-                await deleting_row (
-                    "orders_all_json",
-                    "databases/trading.sqlite3",
-                    where_filter,
-                    "=",
+                len_order_is_reconciled_each_other =  check_whether_order_db_reconciled_each_other (
+                    sub_account,
                     instrument_name,
-                )
+                    orders_currency)
                 
-                for order in sub_account_instrument_name:
+                if not len_order_is_reconciled_each_other:
+                                
+                    sub_account_instrument_name = [o for o in sub_account_orders \
+                        if instrument_name in o["instrument_name"]]
+                            
+                    where_filter = f"instrument_name"
                     
-                    await insert_tables(
-                                order_db_table, 
-                                order
-                                )
-                
-                await modify_order_and_db. resupply_sub_accountdb(currency)
+                    await deleting_row (
+                        "orders_all_json",
+                        "databases/trading.sqlite3",
+                        where_filter,
+                        "=",
+                        instrument_name,
+                    )
+                    
+                    for order in sub_account_instrument_name:
+                        
+                        await insert_tables(
+                                    order_db_table, 
+                                    order
+                                    )
+                    
+                    await modify_order_and_db. resupply_sub_accountdb(currency)
                 
     except Exception as error:
         log.warning(error)
