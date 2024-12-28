dc0cee54de8902955e9978813eff20669b8031a5
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-28 13:48:42 +0700

    .

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -28,14 +28,14 @@ def analysis_based_on_length(
     avg_body_length = np.average(body_length)
     body_length_exceed_average = body_length > avg_body_length
     #print(candles_arrays)
-    log.warning (f"candle_type {candle_type}")
-    log.warning (f"wicks_up {wicks_up}")
-    log.warning (f"wicks_down {wicks_down}")
-    log.warning (f"body_size {body_size}")
-    log.warning (f"body_length {body_length}")
-    log.warning (f"is_long_body {is_long_body}")
-    log.warning (f" avg_body_length {avg_body_length}")
-    log.warning (f" body_length_exceed_average {body_length_exceed_average}")
+    #log.warning (f"candle_type {candle_type}")
+    #log.warning (f"wicks_up {wicks_up}")
+    #log.warning (f"wicks_down {wicks_down}")
+    #log.warning (f"body_size {body_size}")
+    #log.warning (f"body_length {body_length}")
+    #log.warning (f"is_long_body {is_long_body}")
+    #log.warning (f" avg_body_length {avg_body_length}")
+    #log.warning (f" body_length_exceed_average {body_length_exceed_average}")
     
     return  (dict(
             candle_type = candle_type,
