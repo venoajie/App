e3b06439f2ddde5ec6066a0965c2771245bfcb15
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 14:07:00 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -1,25 +1,44 @@
 import pandas as pd
-from time import sleep
-from lightweight_charts import Chart
+import plotly.express as px
+import cufflinks
+import plotly.io as pio 
+import yfinance as yf
+import warnings 
+warnings.filterwarnings("ignore")
+cufflinks.go_offline()
+cufflinks.set_config_file(world_readable=True, theme='pearl')
+pio.renderers.default = "notebook" # should change by looking into pio.renderers
 
-if __name__ == '__main__':
+pd.options.display.max_columns = None
 
-    chart = Chart()
 
-    df1 = pd.read_csv('ohlcv.csv')
-    df2 = pd.read_csv('next_ohlcv.csv')
+symbols = ["AAPL"]
 
-    chart.set(df1)
+df = yf.download(tickers=symbols)
+df.head()   
 
-    chart.show()
+# convert column names into lowercase
+df.columns = [c.lower() for c in df.columns]
+df.rename(columns={"adj close":"adj_close"},inplace=True)
 
-    last_close = df1.iloc[-1]['close']
-    
-    for i, series in df2.iterrows():
-        chart.update(series)
+def moving_average(series, window=5, kind="sma"):
+    if kind=="sma":
+        return series.rolling(window=window, min_periods=window).mean()
+    elif kind=="ema":
+        return series.rolling(window=window, min_periods=window).mean()
+    elif kind=="wma":
+        return series.rolling(window=window, min_periods=window).apply(lambda x: np.average(x, weights=np.arange(1, window+1,1)))
+        
+        
+        
 
-        if series['close'] > 20 and last_close < 20:
-            chart.marker(text='The price crossed $20!')
-            
-        last_close = series['close']
-        sleep(0.1)
+tdf = df.copy()
+
+window=30
+tdf[f"close_sma_{window}"] = moving_average(tdf.close, window=window)
+tdf[f"close_ema_{window}"] = moving_average(tdf.close, window=window, kind="ema")
+tdf[f"close_wma_{window}"] = moving_average(tdf.close, window=window, kind="wma")
+tdf
+
+cols = [c for c in tdf.columns if "close" in c and "adj" not in c]
+tdf[cols].iplot(kind="line")
