5c84eaf53330acef402f7f71922f56a46b9ca7fa
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 17:39:38 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -4,9 +4,6 @@ import warnings
 import numpy as np
 import mplfinance as mpf
 
-warnings.filterwarnings("ignore")
-
-pd.options.display.max_columns = None
 daily = pd.read_csv('SP500_NOV2019_Hist.csv',index_col=0,parse_dates=True)
 daily.index.name = 'Date'
 daily.shape
