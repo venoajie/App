85ad99e8f155203c3b4ab8b52d4797b0fdafbe6d
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 17:49:50 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -10,7 +10,7 @@ bitCoinUSD = yf.Ticker("BTC-USD")
 bitCoinUSD_values = bitCoinUSD.history(start="2024-12-21")
 
 # now plot. For plotting styles see: https://github.com/matplotlib/mplfinance/blob/master/examples/styles.ipynb
-mpf.plot(bitCoinUSD_values,type='candle',volume=True,figratio=(3,1),style='yahoo', title='Bitcoin (USD) from: 21 September 2021');
+mpf.plot(bitCoinUSD_values,type='candle',volume=True,figratio=(3,1),style='yahoo', title='Bitcoin (USD) from: 21 September 2021')
 """
   calculate the short period and long period emas 
   the 20 and 50 here should be changed to whatever timeframes you wish to use
