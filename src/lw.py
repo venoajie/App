052df22cb3881cfca83f5d0bb657004f1045c7f0
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 16:09:05 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -5,35 +5,40 @@ import plotly.io as pio
 import yfinance as yf
 import warnings 
 import numpy as np
+import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 
 pd.options.display.max_columns = None
 
-# get historical data of S&P500(^GSPC)
-df = yf.download("^GSPC", period='1d', start='2019-01-01', end='2022-01-31')
-df.head()
+aapl_df = yf.download("AAPL", start="2018-03-24", end="2023-03-24")
+
 
 # use different colors to distinguish between an up or down day — green for up days, and red for down days. Otherwise the volume bars all have the same color.
 
+aapl_df['ema_short'] = aapl_df['Close'].ewm(span=20, adjust=False).mean()
+aapl_df['ema_long'] = aapl_df['Close'].ewm(span=50, adjust=False).mean()
 
-df['diff'] = df['Close'] - df['Open']
-df.loc[df['diff']>=0, 'color'] = 'green'
-df.loc[df['diff']<0, 'color'] = 'red'
-fig3_b = make_subplots(specs=[[{"secondary_y": True}]])
-fig3_b.add_trace(go.Candlestick(x=df.index,
-                              open=df['Open'],
-                              high=df['High'],
-                              low=df['Low'],
-                              close=df['Close'],
-                             ))
-fig3_b.add_trace(go.Bar(x=df.index, y=df['Volume'], name='Volume', marker={'color':df['color']}),secondary_y=True)
-fig3_b.update_layout(xaxis_rangeslider_visible=False)  # hide rangeslider below the Candlestick Chart
-fig3_b.update_layout(title={'text': 'S&P500', 'x': 0.5})
-fig3_b.update_yaxes(range=[0,5000])
-fig3_b.update_yaxes(range=[0,5e10],secondary_y=True)
-fig3_b.show()
 
+aapl_df['bullish'] = 0.0
+aapl_df['bullish'] = np.where(aapl_df['ema_short'] > aapl_df['ema_long'], 1.0, 0.0)
+aapl_df['crossover'] = aapl_df['bullish'].diff()
+
+fig = plt.figure(figsize=(12,8))
+ax1 = fig.add_subplot(111, ylabel='Price in $')
+
+aapl_df['Close'].plot(ax=ax1, color='b', lw=2.)
+aapl_df['ema_short'].plot(ax=ax1, color='r', lw=2.)
+aapl_df['ema_long'].plot(ax=ax1, color='g', lw=2.)
+
+ax1.plot(aapl_df.loc[aapl_df.positions == 1.0].index, 
+         aapl_df.Close[aapl_df.positions == 1.0],
+         '^', markersize=10, color='g')
+ax1.plot(aapl_df.loc[aapl_df.positions == -1.0].index, 
+         aapl_df.Close[aapl_df.positions == -1.0],
+         'v', markersize=10, color='r')
+plt.legend(['Close', 'EMA Short', 'EMA Long', 'Buy', 'Sell'])
+plt.title('AAPL EMA Crossover')
 # convert column names into lowercase
 df = df.rename(
         columns={
