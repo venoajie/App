08dd7ce6687b464f701eef85f08315224ff93232
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 14:17:35 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -16,10 +16,20 @@ symbols = ["AAPL"]
 
 df = yf.download(tickers=symbols)
 df.head()   
-print(df)
 # convert column names into lowercase
-df.columns = [c.lower() for c in df.columns]
+df = df.rename(
+        columns={
+            "Ticker": "ticker",
+            "Open": "open",
+            "High": "high",
+            "Low": "low",
+            "Close": "close",
+            "Volume": "volume",
+            "adj close": "adj_close",
+        }
+    )
 df.rename(columns={"adj close":"adj_close"},inplace=True)
+print(df)
 
 def moving_average(series, window=5, kind="sma"):
     if kind=="sma":
