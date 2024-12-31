a117046a5362c57d895523dd687e2ff2999c6cf6
venoajie <59521823+venoajie@users.noreply.github.com>
2024-12-31 17:46:54 +0700

    Update lw.py

--- src/lw.py
+++ src/lw.py
@@ -4,16 +4,15 @@ import warnings
 import numpy as np
 import mplfinance as mpf
 
-daily = pd.read_csv('SP500_NOV2019_Hist.csv',index_col=0,parse_dates=True)
-daily.index.name = 'Date'
-daily.shape
-daily.head(3)
-daily.tail(3)
+# obtain the Bitcoin ticker in USD
+bitCoinUSD = yf.Ticker("BTC-USD")
+# save the historical market data to a dataframe
+bitCoinUSD_values = bitCoinUSD.history(start="2021-09-21")
 
+# now plot. For plotting styles see: https://github.com/matplotlib/mplfinance/blob/master/examples/styles.ipynb
+mpf.plot(bitCoinUSD_values,type='candle',volume=True,figratio=(3,1),style='yahoo', title='Bitcoin (USD) from: 21 September 2021');
 """
   calculate the short period and long period emas 
   the 20 and 50 here should be changed to whatever timeframes you wish to use
   the values are added into the same dataframe
 """
-mpf.plot(daily)
-mpf.show()
