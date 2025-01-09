052d597a564b3a363131348e30a6929f9b417379
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-09 09:43:51 +0700

    Update allocating_ohlc.py

--- src/websocket_management/allocating_ohlc.py
+++ src/websocket_management/allocating_ohlc.py
@@ -95,16 +95,18 @@ async def last_open_interest_tick_fr_sqlite(
 async def last_tick_fr_sqlite(last_tick_query_ohlc1) -> int:
     """ """
     try:
-        last_tick1_fr_sqlite = await executing_query_with_return(
+        last_tick1 = await executing_query_with_return(
             last_tick_query_ohlc1
             )
+        
+        print(f" last_tick1 {last_tick1}")
 
     except Exception as error:
         await async_raise_error_message(
             error,
             "Capture market data - failed to fetch last_tick_fr_sqlite",
         )
-    return last_tick1_fr_sqlite[0]["MAX (tick)"]
+    return last_tick1[0]["MAX (tick)"]
 
 async def replace_previous_ohlc_using_fix_data(
     instrument_ticker,
@@ -159,9 +161,12 @@ async def ohlc_result_per_time_frame(
         TABLE_OHLC1
         )
 
+    print (f"last_tick_query_ohlc1 {last_tick_query_ohlc1}")
     last_tick1_fr_sqlite: int = await last_tick_fr_sqlite (last_tick_query_ohlc1)
+    print (f"last_tick1_fr_sqlite {last_tick1_fr_sqlite}")
 
     last_tick_fr_data_orders: int = data_orders ["tick"]
+    print (f"last_tick_fr_data_orders {last_tick_fr_data_orders}")
     
     # refilling current ohlc table with updated data
     refilling_current_ohlc_table_with_updated_streaming_data = last_tick1_fr_sqlite == last_tick_fr_data_orders
