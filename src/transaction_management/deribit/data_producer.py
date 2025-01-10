24fab9d91c8533768f03d29018e2992ff19f32a3
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-11 06:53:19 +0700

    Update data_producer.py

--- src/transaction_management/deribit/data_producer.py
+++ src/transaction_management/deribit/data_producer.py
@@ -188,7 +188,7 @@ class StreamAccountData(ModifyOrderDb):
                     await self.establish_heartbeat()
 
                     # Start Authentication Refresh Task
-                    asyncio.create_task(self.ws_refresh_auth())
+                    await self.ws_refresh_auth()
                                 
                     resolution = 1                
                     
