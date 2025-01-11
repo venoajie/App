14e92a56c4e73b296ca912fbbf04f7acad894537
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-11 18:33:24 +0700

    Update running_strategies.py

--- src/transaction_management/deribit/running_strategies.py
+++ src/transaction_management/deribit/running_strategies.py
@@ -432,7 +432,7 @@ async def executing_strategies(
                                 
                             my_trades= remove_redundant_elements([o["instrument_name"] for o in my_trades_currency ])
                             my_labels= remove_redundant_elements([parsing_label(o["label"])["main"] for o in my_trades_currency ])
-                            
+                            log.critical (orders_all)
                             for label in my_labels: 
                                 log.debug (f"label {label}")
                                 amount = sum([o["amount"] for o in my_trades_currency if label in o["label"]])
