e4c76785cd22cfbfd5419ad1cd8d4da4ce194f0a
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-11 10:18:38 +0700

    Update running_strategies.py

--- src/transaction_management/deribit/running_strategies.py
+++ src/transaction_management/deribit/running_strategies.py
@@ -22,7 +22,15 @@ from utilities.system_tools import (
     parse_error_message,
     provide_path_for_file,)
 
+from transaction_management.deribit.get_instrument_summary import (
+    get_futures_instruments,)
 
+from utilities.string_modification import (
+    extract_currency_from_text,
+    parsing_label,
+    remove_double_brackets_in_list,
+    #remove_list_elements,
+    remove_redundant_elements)
 from utilities.system_tools import (
     parse_error_message,
     provide_path_for_file,)
@@ -111,13 +119,75 @@ async def executing_strategies(
         # parsing config file
         config_app = get_config(file_toml)
 
+       
+        modify_order_and_db: object = ModifyOrderDb(sub_account_id)
 
-        chart_trades_buffer = []
+        # registering strategy config file    
+        file_toml: str = "config_strategies.toml"
+
+        # parsing config file
+        config_app = get_config(file_toml)
+
+        # get tradable strategies
+        tradable_config_app = config_app["tradable"]
+        
+        # get tradable currencies
+        #currencies_spot= ([o["spot"] for o in tradable_config_app]) [0]
+        currencies= ([o["spot"] for o in tradable_config_app]) [0]
+        
+        #currencies= random.sample(currencies_spot,len(currencies_spot))
+        
+        strategy_attributes = config_app["strategies"]
+        
+        strategy_attributes_active = [o for o in strategy_attributes \
+            if o["is_active"]==True]
+                                    
+        active_strategies =   [o["strategy_label"] for o in strategy_attributes_active]
+        
+        # get strategies that have not short/long attributes in the label 
+        non_checked_strategies =   [o["strategy_label"] for o in strategy_attributes_active \
+            if o["non_checked_for_size_label_consistency"]==True]
+        
+        cancellable_strategies =   [o["strategy_label"] for o in strategy_attributes_active \
+            if o["cancellable"]==True]
+        
+        contribute_to_hedging_strategies =   [o["strategy_label"] for o in strategy_attributes_active \
+            if o["contribute_to_hedging"]==True]
         
         relevant_tables = config_app["relevant_tables"][0]
         
-        order_db_table= relevant_tables["orders_table"]        
+        trade_db_table= relevant_tables["my_trades_table"]
         
+        order_db_table= relevant_tables["orders_table"]        
+                        
+        settlement_periods = get_settlement_period (strategy_attributes)
+        
+        futures_instruments = await get_futures_instruments (
+            currencies,
+            settlement_periods,
+            )  
+
+        instrument_attributes_futures_all = futures_instruments["active_futures"]   
+        
+        instrument_attributes_combo_all = futures_instruments["active_combo"]  
+        
+        instruments_name = futures_instruments["instruments_name"]   
+        
+        min_expiration_timestamp = futures_instruments["min_expiration_timestamp"]   
+        
+        # filling currencies attributes
+        my_path_cur = provide_path_for_file("currencies")
+        replace_data(
+            my_path_cur,
+            currencies
+            )
+        
+        #ticker_perpetual = cached_ticker(instruments_name)  
+        
+        #orders_all = combining_order_data(currencies)  
+        
+        #log.warning (f"orders_all {orders_all}")
+         
         while True:
             
             message: str = queue.get()
@@ -144,3 +214,10 @@ async def executing_strategies(
             "general_error"
             )
 
+
+def get_settlement_period (strategy_attributes) -> list:
+    
+    return (remove_redundant_elements(
+        remove_double_brackets_in_list(
+            [o["settlement_period"]for o in strategy_attributes]))
+            )
