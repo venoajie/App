3b00f1d7c1f3a938e7a5989cedfead93ecc3f9e8
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-18 10:45:12 +0700

    Update scanning_volume_spike.py

--- src/market_understanding/scanning_volume_spike.py
+++ src/market_understanding/scanning_volume_spike.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import asyncio
-import json
 import time
-from collections import deque
-from random import sample
-
+import json
+import asyncio
 import httpx
+from random import sample
+from collections import deque
 
+from loguru import logger as log
 from messaging.telegram_bot import telegram_bot_sendtext
 
 headers = [
@@ -15,44 +15,65 @@ headers = [
     'Pings',
     'Net Vol BTC',
     'Net Vol %',
-    'Recent Total Vol BTC',
-    'Recent Vol %',
+    'Recent Total Vol BTC', 
+    'Recent Vol %', 
     'Recent Net Vol',
-    'Datetime (UTC)',
-]
+    'Datetime (UTC)'
+    ]
 
 starttime = time.time()
 
-
 async def scanning_volume():
     """
-
+    
     https://stackoverflow.com/questions/66686458/how-to-scrape-an-updating-html-table-using-selenium
 
     """
-
-    print('Scanning volume')
+    
+    print("Scanning volume")
 
     while True:
-        async with httpx.AsyncClient(
-            headers={'Connection': 'keep-alive'}
-        ) as client:
-
-            response = await client.get(
-                'https://agile-cliffs-23967.herokuapp.com/ok'
-            )
+        async with httpx.AsyncClient(headers={'Connection': 'keep-alive'}) as client:
+            
+            response = await client.get('https://agile-cliffs-23967.herokuapp.com/ok')
 
             response_json = json.loads(response.text)['resu']
-
+            
             rows = [str(i).split('|') for i in response_json[:-1]]
+            
             result = []
             if rows:
+                
                 data_all = [dict(zip(headers, l)) for l in rows]
+                                
+                for single_data in data_all:
+                    
+                    log.warning (f"single_data {single_data}")
+                    
+                    if not result:
+                        data = [o for o in result 
+                                if o["datetime"] == single_data["datetime"] ]
+                    
+                    else:
+                        data = data_all
 
-                await telegram_bot_sendtext(
-                    f'data_all - {data_all}', 'general_error'
+                    log.debug (f"data {data}")
+                    
+                    if not data:
+                        result.append(single_data)
+    
+                        log.debug (f"result {result}")
+                        
+                        await telegram_bot_sendtext (
+                        f"data - {data}",
+                        "general_error"
+                        )
+                
+        random_sleep_time = max(
+            sample(
+                [5,10,15,20,30],
+                1
                 )
-
-        random_sleep_time = max(sample([5, 10, 15, 20, 30], 1))
+                                )
 
         await asyncio.sleep((random_sleep_time))
