0b93d18e3f7bf843ebed6cd5855aa3f4247f5d37
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-18 18:29:00 +0700

    Update scanning_volume_spike.py

--- src/market_understanding/scanning_volume_spike.py
+++ src/market_understanding/scanning_volume_spike.py
@@ -5,9 +5,7 @@ import json
 import asyncio
 import httpx
 from random import sample
-from collections import deque
 
-from loguru import logger as log
 from messaging.telegram_bot import telegram_bot_sendtext
 
 headers = [
@@ -61,17 +59,6 @@ async def scanning_volume():
                             and single_data["Coin"] in o["Coin"]
                             ]
                     )
-                    
-                    #log.error ([
-                    #        o
-                    #        for o in cached_data
-                    #        if int(o["Pings"]) == int(single_data["Pings"])
-                    #        and single_data["Coin"] in o["Coin"]
-                    #        ])
-                    #log.warning (f"data_has_exist_before {data_has_exist_before}")
-                    #log.info (f"data_has_exist_before == [] {data_has_exist_before == []}")
-                    #log.debug (f"single_data {single_data}")
-                    #log.warning (f"cached_data {cached_data}")
 
                     if data_has_exist_before == []:
                         cached_data.append(single_data)
@@ -80,10 +67,7 @@ async def scanning_volume():
                             f"""{single_data}""", 
                             "general_error"
                         )
-                    #log.debug (f"cached_data {cached_data}")
 
         random_sleep_time = max(sample([5, 10, 15, 20, 30], 1))
-        
-        #log.critical (random_sleep_time)
 
         await asyncio.sleep((random_sleep_time))
