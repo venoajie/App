dde12f9d2ede153461afe8fbe89572d0961e631f
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-22 20:23:18 +0700

    Update capturing_user_changes.py

--- src/transaction_management/deribit/capturing_user_changes.py
+++ src/transaction_management/deribit/capturing_user_changes.py
@@ -118,7 +118,8 @@ async def saving_and_relabelling_orders(sub_account_id, config_app: list, queue:
                 else:
                     delta_time = server_time
             
-            await queue.task_done()
+                
+                await queue.task_done()
             
     except Exception as error:
 
