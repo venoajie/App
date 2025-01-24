31cc7354139cfb257a7d733572f4c282daca5b93
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-24 22:29:00 +0700

    Update saving_result.py

--- src/transaction_management/deribit/saving_result.py
+++ src/transaction_management/deribit/saving_result.py
@@ -63,6 +63,8 @@ async def saving_ws_data(queue: object):
             if message_channel == f"incremental_ticker.{instrument_ticker}":
 
                 my_path_ticker: str = provide_path_for_file("ticker", instrument_ticker)
+                
+                log.info (f"my_path_ticker {instrument_ticker} {my_path_ticker}")
 
                 distribute_ticker_result_as_per_data_type(
                     my_path_ticker,
