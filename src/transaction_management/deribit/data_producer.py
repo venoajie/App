702720227fe8dcf6ad31b091a22c02259f8c71e0
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-25 08:59:23 +0700

    Update data_producer.py

--- src/transaction_management/deribit/data_producer.py
+++ src/transaction_management/deribit/data_producer.py
@@ -285,8 +285,8 @@ class StreamAccountData(ModifyOrderDb):
                                 if "user.changes.any" in message_channel:     
                                     
                                     log.warning (f"message_params {message_params}")
-                                    await queue_capturing_user_changes(len_msg)
-                                    await queue_cancelling(len_msg)
+                                    await queue_capturing_user_changes.put(len_msg)
+                                    await queue_cancelling.put(len_msg)
                                     
                                 has_order.release() 
                                 #await queue.put(message_params)
