8ae708086e69703210f112504806bc789b1cc653
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-28 18:16:01 +0700

    Update cancelling_active_orders.py

--- src/transaction_management/deribit/cancelling_active_orders.py
+++ src/transaction_management/deribit/cancelling_active_orders.py
@@ -382,7 +382,7 @@ async def cancelling_orders(
                 continue
                 
             finally:
-                await asyncio.sleep(.01) 
+                await asyncio.sleep(.0001) 
 
             
     except Exception as error:
