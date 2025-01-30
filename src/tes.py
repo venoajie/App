28c50a0031d955b17b179e89bfe7dc06f47a2610
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-30 12:55:01 +0700

    Update tes.py

--- src/tes.py
+++ src/tes.py
@@ -4,21 +4,10 @@ data = [1, 2, 3, 4, 5]
 product = reduce(lambda x, y: x * y, data)  # Calculates
 
 print(product)
-result = ["CRV|2|1.45238055|7.7%|0.99811558|3.4%|0.49015232|21:56:38 01/20/25", 627]
+dt = []
+for dat in data:
+    dt.append(str(dat))
 
-rows = [str((i.replace("\n" and "%", ""))).split("|") for i in result[:-1]]
-print(rows)
-print(result[1])
+    print(dt)
 
-headers = [
-    "coin",
-    "pings",
-    "net_vol_btc",
-    "net_vol_pct",
-    "recent_total_vol_btc",
-    "recent_vol_pct",
-    "recent_net_vol",
-    "datetime",  #  (UTC)
-]
-data_all = [dict(zip(headers, l)) for l in rows]
-print(data_all)
+print(dt)
