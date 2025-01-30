e4694c7d64ee1892f8b40f08278e09d7f16cc145
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-30 18:45:41 +0700

    Update distributing_ws_data.py

--- src/transaction_management/deribit/distributing_ws_data.py
+++ src/transaction_management/deribit/distributing_ws_data.py
@@ -126,6 +126,8 @@ async def caching_distributing_data(
 
         redis_pool = ConnectionPool(host="localhost", port=6379, db=0)
         client_redis = redis.Redis(connection_pool=redis_pool)
+        
+        chart_trade = False
 
         while True:
 
