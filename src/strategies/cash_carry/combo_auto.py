0df65a751ec8a2d9113488df6a0448d1ad4daafa
venoajie <59521823+venoajie@users.noreply.github.com>
2025-01-30 07:21:23 +0700

    Update combo_auto.py

--- src/strategies/cash_carry/combo_auto.py
+++ src/strategies/cash_carry/combo_auto.py
@@ -752,7 +752,7 @@ class ComboAuto(BasicStrategy):
             o for o in orders_currency if instrument_name_future in o["instrument_name"]
         ]
         
-        log.inf (f"orders_currency_strategy {orders_currency}")
+        log.info (f"orders_currency_strategy {orders_currency}")
 
         orders_instrument_future_open_all: list = [
             o
@@ -761,7 +761,7 @@ class ComboAuto(BasicStrategy):
             and "-FS-" not in o["instrument_name"]
             and "open" in o["label"]
         ]
-        log.inf (f"orders_instrument_future_open_all {orders_instrument_future_open_all}")
+        log.info (f"orders_instrument_future_open_all {orders_instrument_future_open_all}")
 
         len_orders_instrument_future_open_all = (
             0
