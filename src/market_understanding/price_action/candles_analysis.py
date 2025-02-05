ff0d6bb0b586aae5cab65b858bc598deb0f71631
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-06 00:07:51 +0700

    Update candles_analysis.py

--- src/market_understanding/price_action/candles_analysis.py
+++ src/market_understanding/price_action/candles_analysis.py
@@ -387,11 +387,15 @@ async def get_market_condition(
                                     
                                     result =  [o for o in [i for i in [y for y in candles_data_instrument
                                 if resolution == y["resolution"]] if instrument_name in i["instrument_name"]
-                        ][0]["ohlc"]]
+                        ][0]["ohlc"]][0]
                                     
                                     log.debug (result)
                                     
-                                                                    
+                                    
+                                    [o for o in [i for i in [y for y in candles_data_instrument
+                                if resolution == y["resolution"]] if instrument_name in i["instrument_name"]
+                        ][0]["ohlc"]][0] = updated_data
+                                    
                                     log.warning(f" ohlc_resolution after {ohlc_resolution}")
                                         
                                 
