8aa1f9914aa7d8e0c786549211e4778469124aef
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-07 11:36:09 +0700

    Update allocating_ohlc.py

--- src/transaction_management/deribit/allocating_ohlc.py
+++ src/transaction_management/deribit/allocating_ohlc.py
@@ -146,6 +146,7 @@ async def ohlc_result_per_time_frame(
     last_tick1_fr_sqlite: int = await last_tick_fr_sqlite(last_tick_query_ohlc1)
 
     log.debug (f"last_tick1_fr_sqlite {last_tick1_fr_sqlite}")
+    log.debug (f"data_orders {data_orders}")
 
     last_tick_fr_data_orders: int = data_orders["tick"]
 
