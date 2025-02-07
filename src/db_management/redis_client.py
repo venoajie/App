f643144e0c17642b1dc727b6bb1c929d7f659492
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-07 16:54:19 +0700

    .

--- src/db_management/redis_client.py
+++ src/db_management/redis_client.py
@@ -16,6 +16,74 @@ asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 from messaging.telegram_bot import telegram_bot_sendtext
 from utilities.system_tools import parse_error_message
 
+# import redis.asyncio as aioredis
+import json
+
+class RedisPubSubManager:
+    """
+    
+    https://medium.com/@nandagopal05/scaling-websockets-with-pub-sub-using-python-redis-fastapi-b16392ffe291
+        Initializes the RedisPubSubManager.
+
+    Args:
+        host (str): Redis server host.
+        port (int): Redis server port.
+    """
+
+    def __init__(self, host='localhost', port=6379):
+        self.redis_host = host
+        self.redis_port = port
+        self.pubsub = None
+
+    async def _get_redis_connection(self) -> aioredis.Redis:
+        """
+        Establishes a connection to Redis.
+
+        Returns:
+            aioredis.Redis: Redis connection object.
+        """
+        return aioredis.Redis(host=self.redis_host,
+                              port=self.redis_port,
+                              auto_close_connection_pool=False)
+
+    async def connect(self) -> None:
+        """
+        Connects to the Redis server and initializes the pubsub client.
+        """
+        self.redis_connection = await self._get_redis_connection()
+        self.pubsub = self.redis_connection.pubsub()
+
+    async def _publish(self, room_id: str, message: str) -> None:
+        """
+        Publishes a message to a specific Redis channel.
+
+        Args:
+            room_id (str): Channel or room ID.
+            message (str): Message to be published.
+        """
+        await self.redis_connection.publish(room_id, message)
+
+    async def subscribe(self, room_id: str) -> aioredis.Redis:
+        """
+        Subscribes to a Redis channel.
+
+        Args:
+            room_id (str): Channel or room ID to subscribe to.
+
+        Returns:
+            aioredis.ChannelSubscribe: PubSub object for the subscribed channel.
+        """
+        await self.pubsub.subscribe(room_id)
+        return self.pubsub
+
+    async def unsubscribe(self, room_id: str) -> None:
+        """
+        Unsubscribes from a Redis channel.
+
+        Args:
+            room_id (str): Channel or room ID to unsubscribe from.
+        """
+        await self.pubsub.unsubscribe(room_id)
 
 class Singleton(type):
     """
--- src/transaction_management/deribit/allocating_ohlc.py
+++ src/transaction_management/deribit/allocating_ohlc.py
@@ -25,57 +25,6 @@ from utilities.string_modification import transform_nested_dict_to_list
 from utilities.system_tools import async_raise_error_message
 
 
-async def recording_multiple_time_frames(
-    instrument_ticker: str,
-    table_ohlc: str,
-    resolution: int,
-    start_timestamp: int,
-    end_timestamp: int,
-) -> float:
-    """ """
-    ONE_SECOND = 1000
-    one_minute = ONE_SECOND * 60
-
-    try:
-        delta = (end_timestamp - start_timestamp) / (one_minute * resolution)
-
-        if delta > 1:
-
-            ohlc_request = await get_ohlc_data(
-                instrument_ticker,
-                resolution,
-                start_timestamp,
-                end_timestamp,
-            )
-
-            result = [
-                o
-                for o in transform_nested_dict_to_list(ohlc_request)
-                if o["tick"] > start_timestamp
-            ][0]
-
-            await insert_tables(table_ohlc, result)
-
-    except Exception as error:
-        await async_raise_error_message(
-            error,
-            "Capture market data - failed to fetch last open_interest",
-        )
-
-
-async def last_open_interest_tick_fr_sqlite(last_tick_query_ohlc1) -> float:
-    """ """
-    try:
-        last_open_interest = await executing_query_with_return(last_tick_query_ohlc1)
-
-    except Exception as error:
-        await async_raise_error_message(
-            error,
-            "Capture market data - failed to fetch last open_interest",
-        )
-    return 0 if last_open_interest == 0 else last_open_interest[0]["open_interest"]
-
-
 async def last_tick_fr_sqlite(last_tick_query_ohlc1) -> int:
     """ """
     try:
@@ -189,38 +138,6 @@ async def ohlc_result_per_time_frame(
 def currency_inline_with_database_address(currency: str, database_address: str) -> bool:
 
     return currency.lower() in str(database_address)
-
-
-async def inserting_open_interest(
-    currency, WHERE_FILTER_TICK, TABLE_OHLC1, data_orders
-) -> None:
-    """ """
-    try:
-
-        if (
-            currency_inline_with_database_address(currency, TABLE_OHLC1)
-            and "open_interest" in data_orders
-        ):
-
-            open_interest = data_orders["open_interest"]
-
-            last_tick_query_ohlc1: str = querying_arithmetic_operator(
-                "tick", "MAX", TABLE_OHLC1
-            )
-
-            last_tick1_fr_sqlite: int = await last_tick_fr_sqlite(last_tick_query_ohlc1)
-
-            await update_status_data(
-                TABLE_OHLC1,
-                "open_interest",
-                last_tick1_fr_sqlite,
-                WHERE_FILTER_TICK,
-                open_interest,
-                "is",
-            )
-
-    except Exception as error:
-        print(f"error allocating ohlc {error}")
 
 
 async def updating_ohlc(
