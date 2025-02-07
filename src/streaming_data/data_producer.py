a6c412820943d7c82601466f447bc14fe450fbab
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-07 18:35:58 +0700

    Update data_producer.py

--- src/streaming_data/data_producer.py
+++ src/streaming_data/data_producer.py
@@ -154,7 +154,9 @@ class StreamingAccountData:
                             await self.ws_operation(
                                 operation="subscribe", ws_channel=ws
                             )
-
+                        
+                        print(f"resolutions {resolutions}")
+                        
                         for resolution in resolutions:
 
                             ws = f"chart.trades.{instrument_perpetual}.{resolution}"
