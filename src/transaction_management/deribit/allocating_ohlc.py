bfebf9e54c0c5f3b3f212b13e8752658932d6ca3
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-09 05:16:19 +0700

    Update allocating_ohlc.py

--- src/transaction_management/deribit/allocating_ohlc.py
+++ src/transaction_management/deribit/allocating_ohlc.py
@@ -71,8 +71,12 @@ async def updating_ohlc(
                     message_byte_data = orjson.loads(message_byte["data"])
 
                     message_channel = message_byte["channel"]
+                    
+                    log.warning(f"message_channel {message_channel}")
 
                     if chart_channel in message_channel:
+                        
+                        log.warning(f"message_channel {message_channel}")
 
                         data = message_byte_data["data"]
 
