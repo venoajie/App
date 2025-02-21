e35506bcb0d8640fe2c81d912f748de5c9906d9c
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-21 07:43:29 +0700

    Update app_data_cleaning.py

--- src/data_cleaning/app_data_cleaning.py
+++ src/data_cleaning/app_data_cleaning.py
@@ -135,7 +135,7 @@ async def reconciling_size(
         portfolio_channel: str = redis_channels["portfolio"]
         my_trades_channel: str = redis_channels["my_trades_cache_updating"]
         sub_account_cached_channel: str = redis_channels["sub_account_cache_updating"]
-        order_allowed_channel: str = redis_channels["is_order_allowed"]
+        order_allowed_channel: str = redis_channels["order_is_allowed"]
 
         # prepare channels placeholders
         channels = [
