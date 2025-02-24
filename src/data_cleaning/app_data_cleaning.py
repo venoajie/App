b9d3db47a6c146f2c9d585d0b94075d6dfe68c84
venoajie <59521823+venoajie@users.noreply.github.com>
2025-02-25 06:43:44 +0700

    .

--- src/data_cleaning/app_data_cleaning.py
+++ src/data_cleaning/app_data_cleaning.py
@@ -7,113 +7,27 @@ import orjson
 from loguru import logger as log
 
 from data_cleaning.managing_closed_transactions import (
-    clean_up_closed_transactions,
-    get_unrecorded_trade_transactions,
-)
-from data_cleaning.managing_delivered_transactions import (
-    is_instrument_name_has_delivered,
-    updating_delivered_instruments,
+    clean_up_closed_transactions
 )
 from data_cleaning.reconciling_db import (
     is_my_trades_and_sub_account_size_reconciled_each_other,
 )
+from db_management.redis_client import publishing_result
 from db_management.sqlite_management import executing_query_with_return
 from messaging.telegram_bot import telegram_bot_sendtext
-from transaction_management.deribit.api_requests import (
-    get_currencies,
-    get_instruments,
-)
 from transaction_management.deribit.orders_management import saving_traded_orders
-from utilities.pickling import read_data, replace_data
 from utilities.string_modification import (
     extract_currency_from_text,
-    remove_double_brackets_in_list,
     remove_redundant_elements,
 )
-from utilities.system_tools import (
-    parse_error_message,
-    provide_path_for_file,
-)
+from utilities.system_tools import parse_error_message
 from utilities.time_modification import get_now_unix_time as get_now_unix
-
-
-def get_settlement_period(strategy_attributes: list) -> list:
-
-    return remove_redundant_elements(
-        remove_double_brackets_in_list(
-            [o["settlement_period"] for o in strategy_attributes]
-        )
-    )
-
-
-def reading_from_pkl_data(
-    end_point: str,
-    currency: str,
-    status: str = None,
-) -> dict:
-    """ """
-
-    path: str = provide_path_for_file(end_point, currency, status)
-
-    return read_data(path)
-
-
-async def get_instruments_from_deribit(currency: str) -> float:
-    """ """
-
-    result = await get_instruments(currency)
-
-    return result
-
-
-async def update_instruments_per_currency(currency):
-
-    instruments = await get_instruments_from_deribit(currency)
-
-    my_path_instruments = provide_path_for_file("instruments", currency)
-
-    replace_data(
-        my_path_instruments,
-        instruments,
-    )
-
-
-async def update_instruments(idle_time: int):
-
-    try:
-
-        while True:
-
-            get_currencies_all = await get_currencies()
-
-            currencies = [o["currency"] for o in get_currencies_all["result"]]
-
-            for currency in currencies:
-
-                await update_instruments_per_currency(currency)
-
-            my_path_cur = provide_path_for_file("currencies")
-
-            replace_data(my_path_cur, currencies)
-
-            await asyncio.sleep(idle_time)
-
-    except Exception as error:
-
-        await telegram_bot_sendtext(
-            f"app data cleaning -reconciling size - {error}",
-            "general_error",
-        )
-
-        parse_error_message(error)
 
 
 async def reconciling_size(
     private_data: object,
     client_redis: object,
-    currencies,
     redis_channels: list,
-    strategy_attributes: list,
     config_app: list,
 ) -> None:
 
@@ -126,13 +40,14 @@ async def reconciling_size(
         order_db_table = relevant_tables["orders_table"]
 
         order_db_table = relevant_tables["orders_table"]
+
         # get redis channels
         order_allowed_channel: str = redis_channels["order_is_allowed"]
         positions_update_channel: str = redis_channels["position_cache_updating"]
         
         # prepare channels placeholders
         channels = [
-            positions_update_channel
+            positions_update_channel,
         ]
 
         # subscribe to channels
@@ -154,118 +69,14 @@ async def reconciling_size(
 
                     if positions_update_channel in message_channel:
 
-                        positions_cached = message_byte_data
-
-                        positions_cached_instrument = remove_redundant_elements(
-                            [o["instrument_name"] for o in positions_cached]
-                        )
-
-
-                        # FROM sub account to other db's
-                        if positions_cached_instrument:
-
-                            # sub account instruments
-                            for instrument_name in positions_cached_instrument:
-
-                                currency: str = extract_currency_from_text(
-                                    instrument_name
-                                )
-
-                                currency_lower = currency.lower()
-                                
-                                archive_db_table = (
-                                    f"my_trades_all_{currency_lower}_json"
-                                )
-
-                                query_trades_all = f"SELECT trade_id, timestamp  FROM  {archive_db_table} WHERE instrument_name LIKE '%{instrument_name}%' ORDER BY timestamp DESC LIMIT 10"
-                                  
-                                my_trades_instrument_name = await executing_query_with_return(query_trades_all)   
-
-                                log.info(f"trades_all {my_trades_instrument_name}")
-
-                                last_10_timestamp_log = [o["timestamp"] for  o in my_trades_instrument_name]
-                                log.info(f"last_10_timestamp_log {last_10_timestamp_log}")
-
-                                timestamp_log = min(last_10_timestamp_log)
-                                
-                                trades_from_exchange = await private_data.get_user_trades_by_instrument_and_time(
-                                                instrument_name,
-                                                timestamp_log
-                                                - 10,  # - x: arbitrary, timestamp in trade and transaction_log not always identical each other
-                                                1000,
-                                            )
-
-                                await agreeing_trades_from_exchange_to_db_based_on_latest_timestamp(
-                                    trades_from_exchange,
-                                    my_trades_instrument_name,
-                                    archive_db_table,
-                                    order_db_table,
-                                    )
-                                
-                                  
-                                query_trades_all_basic = f"SELECT instrument_name, label, amount_dir as amount, trade_id  FROM  {archive_db_table}"
-                                  
-                                query_trades_all_where = f"WHERE instrument_name LIKE '%{instrument_name}%' AND is_open = 1"
-                                  
-                                query_trades_all = f"{query_trades_all_basic} {query_trades_all_where}"
-                                
-                                my_trades_instrument_name = await executing_query_with_return(query_trades_all)   
-                     
-                                await clean_up_closed_transactions(
-                                    archive_db_table,
-                                    my_trades_instrument_name,
-                                )
-
-
-
-                                # eliminating combo transactions as they're not recorded in the book
-                                if "-FS-" not in instrument_name:
-
-                                    my_trades_and_sub_account_size_reconciled = is_my_trades_and_sub_account_size_reconciled_each_other(
-                                        instrument_name,
-                                        my_trades_instrument_name,
-                                        positions_cached,
-                                    )
-
-                                    if not my_trades_and_sub_account_size_reconciled:
-
-                                        timestamp_log = min(
-                                            [
-                                                o["timestamp"]
-                                                for o in my_trades_instrument_name
-                                            ]
-                                        )
-
-                                        ONE_SECOND = 1000
-
-                                        one_minute = ONE_SECOND * 60
-
-                                        end_timestamp = get_now_unix()
-
-                                        five_days_ago = end_timestamp - (
-                                            one_minute * 60 * 24 * 5
-                                        )
-
-                                        timestamp_log = five_days_ago
-
-                                        log.critical(
-                                            f"timestamp_log {timestamp_log}"
-                                        )
-
-                                        trades_from_exchange = await private_data.get_user_trades_by_instrument_and_time(
-                                            instrument_name,
-                                            timestamp_log
-                                            - 10,  # - x: arbitrary, timestamp in trade and transaction_log not always identical each other
-                                            1000,
-                                        )
-
-                                        await update_trades_from_exchange_based_on_latest_timestamp(
-                                            trades_from_exchange,
-                                            instrument_name,
-                                            my_trades_instrument_name,
-                                            archive_db_table,
-                                            order_db_table,
-                                        )
+                        await every_update_on_position_channels(
+                            private_data,
+                            client_redis,
+                            order_allowed_channel,
+                            message_byte_data,
+                            order_db_table,
+                            order_allowed,
+                            )
 
             except Exception as error:
 
@@ -297,9 +108,6 @@ async def update_trades_from_exchange_based_on_latest_timestamp(
 
     if trades_from_exchange:
 
-        log.error(f"trades_from_exchange {trades_from_exchange}")
-        log.debug(f"my_trades_instrument_name {my_trades_instrument_name}")
-
         trades_from_exchange_without_futures_combo = [
             o for o in trades_from_exchange if f"-FS-" not in o["instrument_name"]
         ]
@@ -310,10 +118,6 @@ async def update_trades_from_exchange_based_on_latest_timestamp(
         )
 
         for trade in trades_from_exchange_without_futures_combo:
-
-            log.warning(
-                f"not my_trades_instrument_name {not my_trades_instrument_name}"
-            )
 
             if not my_trades_instrument_name:
 
@@ -328,8 +132,6 @@ async def update_trades_from_exchange_based_on_latest_timestamp(
                 ]
 
                 trade = trade_timestamp[0]
-
-                log.error(f"{trade}")
 
                 await saving_traded_orders(
                     trade,
@@ -347,8 +149,6 @@ async def update_trades_from_exchange_based_on_latest_timestamp(
                     if trade_trd_id in o["trade_id"]
                 ]
 
-                log.debug(f"trade_trd_id_not_in_archive {trade_trd_id_not_in_archive}")
-
                 if not trade_trd_id_not_in_archive:
 
                     log.debug(f"{trade_trd_id}")
@@ -359,8 +159,6 @@ async def update_trades_from_exchange_based_on_latest_timestamp(
                         order_db_table,
                     )
 
-
-
 async def agreeing_trades_from_exchange_to_db_based_on_latest_timestamp(
     trades_from_exchange,
     my_trades_instrument_name,
@@ -370,9 +168,6 @@ async def agreeing_trades_from_exchange_to_db_based_on_latest_timestamp(
     """ """
 
     if trades_from_exchange:
-
-        log.error(f"trades_from_exchange {trades_from_exchange}")
-        log.debug(f"my_trades_instrument_name {my_trades_instrument_name}")
 
         trades_from_exchange_without_futures_combo = [
             o for o in trades_from_exchange if f"-FS-" not in o["instrument_name"]
@@ -397,3 +192,142 @@ async def agreeing_trades_from_exchange_to_db_based_on_latest_timestamp(
                     archive_db_table,
                     order_db_table,
                 )
+
+
+async def every_update_on_position_channels(
+    private_data: object,
+    client_redis: object,
+    order_allowed_channel: str,
+    positions_cached: list,
+    order_db_table: str,
+    order_allowed: bool,
+) -> None:
+    """ """
+
+    positions_cached_instrument = remove_redundant_elements(
+        [o["instrument_name"] for o in positions_cached]
+    )
+
+    # FROM sub account to other db's
+    if positions_cached_instrument:
+
+        # sub account instruments
+        for instrument_name in positions_cached_instrument:
+
+            currency: str = extract_currency_from_text(
+                instrument_name
+            )
+
+            currency_lower = currency.lower()
+            
+            archive_db_table = (
+                f"my_trades_all_{currency_lower}_json"
+            )
+                
+            query_trades_all_basic = f"SELECT trade_id, timestamp  FROM  {archive_db_table}"
+                
+            query_trades_all_where = f"WHERE instrument_name LIKE '%{instrument_name}%' ORDER BY timestamp DESC LIMIT 10"
+                
+            query_trades_all = f"{query_trades_all_basic} {query_trades_all_where}"
+
+            my_trades_instrument_name = await executing_query_with_return(query_trades_all)   
+
+            last_10_timestamp_log = [o["timestamp"] for  o in my_trades_instrument_name]
+
+            timestamp_log = min(last_10_timestamp_log)
+            
+            trades_from_exchange = await private_data.get_user_trades_by_instrument_and_time(
+                            instrument_name,
+                            timestamp_log
+                            - 10,  # - x: arbitrary, timestamp in trade and transaction_log not always identical each other
+                            1000,
+                        )
+
+            await agreeing_trades_from_exchange_to_db_based_on_latest_timestamp(
+                trades_from_exchange,
+                my_trades_instrument_name,
+                archive_db_table,
+                order_db_table,
+                )
+                            
+            query_trades_active_basic = f"SELECT instrument_name, label, amount_dir as amount, trade_id  FROM  {archive_db_table}"
+                
+            query_trades_active_where = f"WHERE instrument_name LIKE '%{instrument_name}%' AND is_open = 1"
+                
+            query_trades_active = f"{query_trades_active_basic} {query_trades_active_where}"
+            
+            my_trades_instrument_name = await executing_query_with_return(query_trades_active)   
+    
+            await clean_up_closed_transactions(
+                archive_db_table,
+                my_trades_instrument_name,
+            )
+
+            # eliminating combo transactions as they're not recorded in the book
+            if "-FS-" not in instrument_name:
+
+                my_trades_and_sub_account_size_reconciled = is_my_trades_and_sub_account_size_reconciled_each_other(
+                    instrument_name,
+                    my_trades_instrument_name,
+                    positions_cached,
+                )
+
+                if not my_trades_and_sub_account_size_reconciled:
+
+                    timestamp_log = min(
+                        [
+                            o["timestamp"]
+                            for o in my_trades_instrument_name
+                        ]
+                    )
+
+                    ONE_SECOND = 1000
+
+                    one_minute = ONE_SECOND * 60
+
+                    end_timestamp = get_now_unix()
+
+                    five_days_ago = end_timestamp - (
+                        one_minute * 60 * 24 * 5
+                    )
+
+                    timestamp_log = five_days_ago
+
+                    trades_from_exchange = await private_data.get_user_trades_by_instrument_and_time(
+                        instrument_name,
+                        timestamp_log
+                        - 10,  # - x: arbitrary, timestamp in trade and transaction_log not always identical each other
+                        1000,
+                    )
+
+                    await update_trades_from_exchange_based_on_latest_timestamp(
+                        trades_from_exchange,
+                        instrument_name,
+                        my_trades_instrument_name,
+                        archive_db_table,
+                        order_db_table,
+                    )
+                    
+                    my_trades_instrument_name = await executing_query_with_return(query_trades_active)   
+
+                else:
+                    
+                    order_allowed = 1
+
+                    pub_message = dict(
+                        instrument_name=instrument_name,
+                        order_allowed=order_allowed,
+                        currency=currency,
+                    )            
+                            
+                    await publishing_result(
+                        client_redis,
+                        order_allowed_channel,
+                        pub_message,
+                    )
+
+                await clean_up_closed_transactions(
+                    archive_db_table,
+                    my_trades_instrument_name,
+                )
+            
