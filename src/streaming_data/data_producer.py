9fa4dc0c7373d69ecfd2090cfb4a0bddc2470232
venoajie <59521823+venoajie@users.noreply.github.com>
2025-03-09 17:59:13 +0700

    Update data_producer.py

--- src/streaming_data/data_producer.py
+++ src/streaming_data/data_producer.py
@@ -123,6 +123,8 @@ class StreamingAccountData:
                         # Receive WebSocket messages
                         message: bytes = await self.websocket_client.recv()
                         message: dict = orjson.loads(message)
+                        
+                        print(message)
 
                         if "id" in list(message):
                             if message["id"] == 9929:
